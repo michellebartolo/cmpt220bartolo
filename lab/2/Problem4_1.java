ebbd424157b8b03787fb1afe5a874fa4ee6dcf77
michellebartolo <michelle.bartolo1@marist.edu>
2017-02-25 17:21:26 -0500

    Update Problem4_1.java

--- lab/2/Problem4_1.java
+++ lab/2/Problem4_1.java
@@ -1,29 +1,29 @@
-//Michelle Bartolo Lab 2
-// Problem 4.1 (Geometry: area of a pentagon) Write a program that prompts the user to enter
-//the length from the center of a pentagon to a vertex and computes the area of the pentagon.
-// Problem 4.1 (Geometry: area of a pentagon) Write a program that prompts the user to enter
-//the length from the center of a pentagon to a vertex and computes the area of the pentagon.
+ /**
+   * file: Problem4_1.java
+   * author: Michelle Bartolo
+   * course: CMPT 220
+   * assignment: Lab 2
+   * due date: February 9, 2017
+   * version: 1.3
+   * 
+   * This file contains the solution the Problem 4.1, which asks to write a program that prompts the user to enter
+   * the length from the center of a pentagon to a vertex and computes the area of the pentagon.
+   */
 
 import java.util.Scanner;
 
 public class PentagonCalculator {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
 	  
-	Scanner input = new Scanner(System.in);
-	
-    //Prompts the user to enter the length of the pentagon from the center to a vertex
-	System.out.println("Enter the length from the center to a vertex: ");
-	double r = input.nextDouble();
-	
-	//compute the length of a side using r
-	double s = ( 2.0  * r ) * (Math.sin( (0.2) * Math.PI) );
-	
-	//Using the side s we just calculated, the area of the pentagon will be found.
-	double area = ( ( 5.0 * (s * s) ) / ( 4.0 * Math.tan( (0.2) * (Math.PI) ) ) ) ; 
-	
-	double roundedarea = Math.round(area * 100.0) / 100.0;
-	
-	//Prints the area of the pentagon
-	System.out.println("The area of the pentagon is " + roundedarea );
+      //Prompts the user to enter the length of the pentagon from the center to a vertex
+      System.out.println("Enter the length from the center to a vertex: ");
+	  
+      double r = input.nextDouble(); 
+      double s = ( 2.0  * r ) * (Math.sin( (0.2) * Math.PI) ); //compute the length of a side using r
+      double area = ( ( 5.0 * (s * s) ) / ( 4.0 * Math.tan( (0.2) * (Math.PI) ) ) ) ; //Using the side s we just calculated, the area of the pentagon will be found.
+      double roundedarea = Math.round(area * 100.0) / 100.0;
+     
+      System.out.println("The area of the pentagon is " + roundedarea ); //Prints the area of the pentagon
   }
 }
