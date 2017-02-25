049ddeba943e625a0a72bfa192533cafde936f34
michellebartolo <michelle.bartolo1@marist.edu>
2017-02-25 17:23:48 -0500

    Update Problem4_5.java

--- lab/2/Problem4_5.java
+++ lab/2/Problem4_5.java
@@ -1,27 +1,32 @@
-//Michelle Bartolo Lab 2
-//Problem 4.5 
-//A regular polygon is an n-sided polygon in which all sides are of the same length and all angles have the same degree 
-//(i.e., the polygon is both equilateral and equiangular). 
+ /**
+   * file: Problem4_5.java
+   * author: Michelle Bartolo
+   * course: CMPT 220
+   * assignment: Lab 2
+   * due date: February 9, 2017
+   * version: 1.3
+   * 
+   * This file contains the solution the Problem 4.5, which asks tofind the area of a regular polygon 
+   */
 
 import java.util.Scanner;
 
 public class PolygonArea {
-  public static void main(String[] args) {
-		  
+  public static void main(String[] args) {	  
     Scanner input = new Scanner(System.in);
+
+    //Prompts the user to enter the number of sides the polygon has
+    System.out.println("Enter the number of sides: ");
+    double n = input.nextDouble();
 		
-	  //Prompts the user to enter the number of sides the polygon has
-      System.out.println("Enter the number of sides: ");
-	  double n = input.nextDouble();
+    //Prompts the user to enter the length of the sides of the polygon 
+    System.out.println("Enter the length of the sides: ");
+    double s = input.nextDouble();
 		
-	  //Prompts the user to enter the length of the sides of the polygon 
-      System.out.println("Enter the length of the sides: ");
-      double s = input.nextDouble();
+    //computes area
+    double area = ( ( n * (s * s) ) / ( 4.0 * ( Math.tan( ( (Math.PI) / n ) ) ) ) ); 
 		
-	  //computes area
-      double area = ( ( n * (s * s) ) / ( 4.0 * ( Math.tan( ( (Math.PI) / n ) ) ) ) ); 
-		
-      //Prints the area of the polygon
-	  System.out.println("The area of the polygon is " + area );
+    //Prints the area of the polygon
+    System.out.println("The area of the polygon is " + area );
   }
 }
