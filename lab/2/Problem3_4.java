6a15593df22adb9a6632307e6e1fc4c1ff851440
michellebartolo <michelle.bartolo1@marist.edu>
2017-02-25 16:54:31 -0500

    Update Problem3_4.java

--- lab/2/Problem3_4.java
+++ lab/2/Problem3_4.java
@@ -1,8 +1,15 @@
-//Michelle Bartolo Lab 2
-//Problem 3.4
-//(Random month) Write a program that randomly generates an integer between 1
-//and 12 and displays the English month name January, February, …, December for
-//the number 1, 2, …, 12, accordingly
+ /**
+   * file: Problem3_15.java
+   * author: Michelle Bartolo
+   * course: CMPT 220
+   * assignment: Lab 2
+   * due date: February 9, 2017
+   * version: 1.3
+   * 
+   * This file contains the solution the Problem 3.4, which asks to write a program that randomly generates an integer between 1
+   * and 12 and displays the English month name January, February, …, December for
+   * the number 1, 2, …, 12, accordingly
+   */
 
 import java.util.Scanner;
 
@@ -12,7 +19,7 @@ public class Problem3_4 {
   //Prints a random number from 1 to 12
   int month = (int)( (Math.random() * 12) + 1 );
   
-	//matches number to month using if else statements
+    //matches number to month using if else statements
     if ( month == 1 ) {
       System.out.println("January");
     } 
@@ -34,13 +41,13 @@ public class Problem3_4 {
     else if ( month == 7 ) {
       System.out.println("July");
     } 
-    else if ( month == 8 ) {
+    else if ( month == 8 ){
       System.out.println("August");
     } 
-    else if ( month == 9 ) {
+    else if ( month == 9 ){ 
       System.out.println("September");
     } 
-    else if ( month == 10 ) {
+    else if ( month == 10 ){
       System.out.println("October");
     } 
     else if ( month == 11 ) {
@@ -49,7 +56,7 @@ public class Problem3_4 {
     else if ( month == 12 ) {
       System.out.println("December");
     }
-	else {
+    else {
       System.out.println("Sorry, please try again.")
     }
   }
