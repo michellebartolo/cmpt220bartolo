513f5ef8e9327569b240a02e91851c2e5abd877a
michellebartolo <michelle.bartolo1@marist.edu>
2017-02-25 12:26:59 -0500

    Update Problem5_1.java

--- lab/3/Problem5_1.java
+++ lab/3/Problem5_1.java
@@ -6,8 +6,7 @@
    * due date: February 23, 2017
    * version: 1.3
    * 
-   * This file contains the declaration of the SinglyLinkedList abstract data
-   * type.
+   * This file contains the solution for Problem 5.1 
    */
 //*5.1 (Count positive and negative numbers and compute the average of numbers) Write
 //a program that reads an unspecified number of integers, determines how many
@@ -19,18 +18,18 @@ import java.util.Scanner;
 
 public class Problem5_1 {
   public static void main(String[] args) {
-  	Scanner input = new Scanner(System.in); // Create new Scanner object
+    Scanner input = new Scanner(System.in); // Create new Scanner object
     //initialize/ declare variables
-    int sum = 0;
-    int positive = 0;
-    int negative = 0;
-    double total=0.0 ;
-    int number;
-    double average = 0.0;
+      int sum = 0;
+      int positive = 0;
+      int negative = 0;
+      double total=0.0 ;
+      int number;
+      double average = 0.0;
 	
     // Keeps "doing" the statements until the input is 0
     do {
-	System.out.println("Enter an integer, the input ends if it is 0: "); //Prompts the user the to enter integers
+      System.out.println("Enter an integer, the input ends if it is 0: "); //Prompts the user the to enter integers
       number = input.nextInt(); //Allows user to input an integer and store it in the number variable
       total++; //Increments by 1
       sum += number; //sum is equal to sum + number
@@ -41,11 +40,11 @@ public class Problem5_1 {
         negative++; //if the number is less than zero, add one to the negative number amount
       }
     } while (number != 0); //continue while the inputted number is not equal to zero 
-    
+	
     average = (sum / (total - 1.0 )); //find the average of the inputted numbers by taking their sum and dividing it by the total minus one because we do not want to include the zero the user inputted 
     System.out.println("The number of positive integers is " + positive); //print out the amount of positive integers
     System.out.println("The number of negative integers is " + negative); //print out the amount of negative integers
     System.out.println("The total is " + total); //print out the total number of integers inputted
-	System.out.println("The average is " + average); //print out the average of the integers inputted
+    System.out.println("The average is " + average); //print out the average of the integers inputted
   }
 }
