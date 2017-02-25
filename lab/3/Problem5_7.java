7d373d44b08540bd3357154193bc8c0059cbba2f
michellebartolo <michelle.bartolo1@marist.edu>
2017-02-25 12:36:12 -0500

    Update Problem5_7.java

--- lab/3/Problem5_7.java
+++ lab/3/Problem5_7.java
@@ -1,24 +1,20 @@
 /**
-   * file: Problem5_7.java
-   * author: Michelle Bartolo
-   * course: CMPT 220
-   * assignment: Lab 3
-   * due date: February 23, 2017
-   * version: 1.3
-   * 
-   * This file contains the declaration of the SinglyLinkedList abstract data
-   * type.
-   */
-
-//**5.7 (Financial application: compute future tuition) Suppose that the tuition for a university is $10,000 this year and increases 5% every year. 
-//In one year, the tuition will be $10,500. Write a program that computes the tuition in ten years and the total cost of four years’ worth of tuition
-//after the tenth year.
+  * file: Problem5_7.java
+  * author: Michelle Bartolo
+  * course: CMPT 220
+  * assignment: Lab 3
+  * due date: February 23, 2017
+  * version: 1.3
+  * 
+  * This file contains the solution for problem 5.7 which asks to write a program that computes the tuition in ten years and the total cost
+  * of four years’ worth of tuition after the tenth year.
+  */
 
 public class Problem5_7 {
   public static void main(String[] args) {
 	
-	//declare variables
-	double tuition = 10000.00;
+    //declare variables
+    double tuition = 10000.00;
     int year = 0;
     double sum = 0;
     
@@ -26,12 +22,12 @@ public class Problem5_7 {
     for (year = 1; year < 14; year++){
       tuition += tuition * .05; //tuition equals itself plus itself times .05
       double roundtuition = (Math.round(tuition * 100.0) / 100.0); //round to tens place
-     if (year == 9){ //if loop to end iterations for year price
+    if (year == 9){ //if loop to end iterations for year price
       System.out.println("The tuition in 10 years will be $" + roundtuition) ; 
     }
-     if (year > 9 && year < 14){ //if loop for incrementing full tuition
-       sum += roundtuition; //accumulate sum by adding sum to tuition 
-     }
+    if (year > 9 && year < 14){ //if loop for incrementing full tuition
+      sum += roundtuition; //accumulate sum by adding sum to tuition 
+    }
     }
     System.out.println("The total four year tuition after 10 years is $" + sum); //print out the sum of the total four year tuition after 10 years
   }
