1a843db4c0a3de21fdc8ca1d4fe9b30c32c961bb
michellebartolo <michelle.bartolo1@marist.edu>
2017-02-25 12:23:46 -0500

    Update Driver_lab3.java

--- lab/3/Driver_lab3.java
+++ lab/3/Driver_lab3.java
@@ -6,8 +6,7 @@
    * due date: February 23, 2017
    * version: 1.3
    * 
-   * This file contains the declaration of the SinglyLinkedList abstract data
-   * type.
+   * This file contains the the Driver_lab3.java file that was submitted to kattis
    */
 import java.util.Scanner;
 
@@ -18,7 +17,7 @@ public class Driver_lab3 {
     //while loop for output iteration	
     while(true) {
       double x1 = s.nextDouble();
-	//if loop if if x not 0 continues computation		
+    //if loop if if x not 0 continues computation		
     if(x1 == 0.0) {
       break;
     }
@@ -28,7 +27,7 @@ public class Driver_lab3 {
     double p = s.nextDouble();  		
     double answer = Math.pow(Math.pow(Math.abs(x1 - x2), p) + (Math.pow(Math.abs(y1 - y2),p)) , (1/p)); //Find the distance
 	  
-	System.out.println(answer); //Print out the distance 
+    System.out.println(answer); //Print out the distance 
     }
   }
 }
