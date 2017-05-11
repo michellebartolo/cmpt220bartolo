aa28fede0e63fbd01054dd85d0d8198416e6edb3
michellebartolo <michelle.bartolo1@marist.edu>
2017-05-11 16:20:38 -0400

    Update Problem1_3.java

--- lab/1/Problem1_3.java
+++ lab/1/Problem1_3.java
@@ -1,16 +1,26 @@
-//Michelle Bartolo Lab 1 1/25/17
+/**
+  * file: Problem1_3.java
+  * author: Michelle Bartolo
+  * course: CMPT 220
+  * assignment: Lab 1
+  * due date: January 25, 2017
+  * version: 1.3
+  * 
+  * This file contains the solution to problem 1.3
+  */
+   
 public class Problem1_3 {
   public static void main(String[] args){
-    /* Print newline before displaying the pattern. */
-    System.out.println();
+	  
+    System.out.println();//Print newline before displaying the pattern
     
-    /* Print the pattern. */
+    //Print the pattern
     System.out.println("    J    A     V     V    A");
     System.out.println("    J   A A     V   V    A A");
     System.out.println("J   J  AAAAA     V V    AAAAA");
     System.out.println(" JJ   A     A     V    A     A");
     
-    /* Print newline after displaying the pattern. */
-    System.out.println();
+    
+    System.out.println(); //print newline after displaying the pattern. 
   }
 }
