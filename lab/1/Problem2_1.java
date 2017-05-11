dccf7d584da9121c1af4e38c54b8da8ad621040b
michellebartolo <michelle.bartolo1@marist.edu>
2017-05-11 16:26:13 -0400

    Update Problem2_1.java

--- lab/1/Problem2_1.java
+++ lab/1/Problem2_1.java
@@ -1,20 +1,29 @@
-//Michelle Bartolo Lab 1- Problem 2.1 1/25/17
+/**
+  * file: Problem2_1.java
+  * author: Michelle Bartolo
+  * course: CMPT 220
+  * assignment: Lab 1
+  * due date: January 25, 2017
+  * version: 1.3
+  * 
+  * This file contains the solution to problem 2.1
+  */
+
 import java.util.Scanner;
 
 public class Problem2_1 {
-	public static void main(String[] args) {
-		// Create new Scanner object
-		Scanner input = new Scanner(System.in);
+  public static void main(String[] args) {
+    // Create new Scanner object
+    Scanner input = new Scanner(System.in);
 
-		// Prompt user to input a double
-		System.out.print("Enter a degree in Celsius: ");
-		double celsius = input.nextDouble();
+    // Prompt user to input a double
+    System.out.print("Enter a degree in Celsius: ");
+    double celsius = input.nextDouble();
 
-		// Convert Celsius to Fahrenheit
-		double fahrenheit = 9.0 / 5 * celsius + 32;
+    // Convert Celsius to Fahrenheit
+    double fahrenheit = 9.0 / 5 * celsius + 32;
 
-		// Display results
-		System.out.println(celsius + " Celsius is " + fahrenheit
-			+ " Fahrenheit"); 
-	}
+    // Display results
+    System.out.println(celsius + " Celsius is " + fahrenheit + " Fahrenheit"); 
+  }
 }
